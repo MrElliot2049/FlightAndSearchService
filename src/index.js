16c5c5e0b8f7bcd8a152c26fe51cadd3035fa240
Adamya Chauhan <adamya1332004@gmail.com>
2026-04-03 19:23:14 +0530

    City Db Done

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 const express = require('express');
-const {PORT} = require('./config/serverConfig');
+const { PORT } = require('./config/serverConfig');
 const bodyParser = require('body-parser');
 const ApiRoutes = require('./routes/index');
 
 const SetupAndStartSever = async () => {
     // app object
     const app = express();
-    app.use(bodyParser.urlencoded({extended:true}));
     app.use(bodyParser.json());
-    
+    app.use(bodyParser.urlencoded({ extended: true }));
+
+
     app.use('/api', ApiRoutes);
     app.listen(PORT, () => {
         console.log(`Server started listening on ${PORT}`);
--- src/repository/city-repository.js
+++ src/repository/city-repository.js
@@ -28,11 +28,14 @@ class CityRepository {
 
     async updateCity(cityId, data) {
         try {
-            const city = City.update(data, {
-                where : {
-                    id: cityId
-                }
-            });
+            // const city = City.update(data, {
+            //     where : {
+            //         id: cityId
+            //     }
+            // });
+            const city = await City.findByPk(cityId);
+            city.name = data.name;
+            await city.save();
             return city;
         } catch (error) {
             console.log("Something went wrong in the repository layer");
