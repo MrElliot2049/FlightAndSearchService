34d807356e882da5ff736462f37f1fd2862404b6
Adamya Chauhan <adamya1332004@gmail.com>
2026-04-03 20:08:08 +0530

    GetAll done

--- src/controllers/city-controller.js
+++ src/controllers/city-controller.js
@@ -85,9 +85,30 @@ const update = async (req, res) => {
     }
 }
 
+const getAll = async (req, res) => {
+    try {
+        const cities = await cityService.getAllCities();
+        return res.status(200).json({
+            data : cities,
+            success: true,
+            message : "Successfully got all cities",
+            err: {}
+        })
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            data : {},
+            success: false,
+            message : "Error in getting all cities",
+            err: error
+        })
+    }
+}
+
 module.exports = {
     create,
     destroy,
     get,
-    update
+    update,
+    getAll
 }
--- src/services/city-service.js
+++ src/services/city-service.js
@@ -44,5 +44,15 @@ class CityService {
             throw {error};
         }
     }
+
+    async getAllCities() {
+        try {
+            const cities = await this.cityRepository.getAllCities();
+            return cities;
+        } catch (error) {
+            console.log("Something went wrong at the service layer");
+            throw {error};
+        }
+    }
 }
 module.exports = CityService;
