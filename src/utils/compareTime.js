7402a12c607c0d4a6a00a31521de2fa7e090550a
Adamya Chauhan <adamya1332004@gmail.com>
2026-04-11 16:56:16 +0530

    added flight filter here

--- src/utils/compareTime.js
+++ src/utils/compareTime.js
@@ -1,6 +1,6 @@
 function compareTime(time1, time2) {
-    const t1 = new Date(time1);
-    const t2 = new Date(time2);
+    let t1 = new Date(time1);
+    let t2 = new Date(time2);
 
     if (t1.getTime() > t2.getTime) return false;
     return true;
